ce1e6fb4a47284884f8d58ef50228ffe74c883fe
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2025-04-12 11:13:34 -0300

    feat: Implementação de ListAsync e GetMessagesAsync

--- LagRabbitMqManagerToolkit/Requests/Interfaces/IQueueRequest.cs
+++ LagRabbitMqManagerToolkit/Requests/Interfaces/IQueueRequest.cs
@@ -4,6 +4,10 @@ namespace LagRabbitMqManagerToolkit.Requests.Interfaces
 {
     public interface IQueueRequest
     {
+        Task<IList<Queue>> ListAsync();
+        
         Task<Queue?> GetAsync(string vHost, string queue);
+
+        Task<IList<Message>> GetMessagesAsync(string vHost, string queue, int take = 200);
     }
 }
--- LagRabbitMqManagerToolkit/Requests/QueueRequest.cs
+++ LagRabbitMqManagerToolkit/Requests/QueueRequest.cs
@@ -8,13 +8,48 @@ namespace LagRabbitMqManagerToolkit.Requests
     {
         public async Task<Queue?> GetAsync(string vHost, string queue)
         {
+            ArgumentException.ThrowIfNullOrEmpty(vHost);
+            ArgumentException.ThrowIfNullOrEmpty(queue);
+
             var endpoint = RabbitEndpoints.GetQueue(vHost, queue);
-            
+
             var url = new Uri(new Uri(_settings.Url), endpoint);
 
             var token = RabbitRequestExtensions.BasicToken(_settings);
-            
+
             return await RabbitRequestExtensions.Get<Queue>(url, token);
+        }
+
+        public async Task<IList<Message>> GetMessagesAsync(string vHost, string queue, int take = 200)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(vHost);
+            ArgumentException.ThrowIfNullOrEmpty(queue);
+
+            var endpoint = RabbitEndpoints.GetQueueMessages(vHost, queue);
+
+            var url = new Uri(new Uri(_settings.Url), endpoint);
+
+            var body = new
+            {
+                ackmode = "ack_requeue_true",
+                encoding = "auto",
+                count = take
+            };
+
+            var token = RabbitRequestExtensions.BasicToken(_settings);
+
+            return await RabbitRequestExtensions.Post<IList<Message>>(url, token, body) ?? [];
+        }
+
+        public async Task<IList<Queue>> ListAsync()
+        {
+            var endpoint = RabbitEndpoints.Queues;
+
+            var url = new Uri(new Uri(_settings.Url), endpoint);
+
+            var token = RabbitRequestExtensions.BasicToken(_settings);
+
+            return await RabbitRequestExtensions.Get<List<Queue>>(url, token) ?? [];
         }
     }
 }
