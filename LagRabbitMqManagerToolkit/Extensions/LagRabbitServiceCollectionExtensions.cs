51e764ec0baa137c4d97f9c403dd5d0422966848
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2025-04-12 06:34:53 -0300

    feat: Dependency Injection

---
+++ LagRabbitMqManagerToolkit/Extensions/LagRabbitServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using LagRabbitMqManagerToolkit.Domains;
+using LagRabbitMqManagerToolkit.Requests.Interfaces;
+using LagRabbitMqManagerToolkit.Requests;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LagRabbitMqManagerToolkit.Extensions
+{
+    public static class LagRabbitServiceCollectionExtensions
+    {
+        public static IServiceCollection AddLagRabbitMqManagerToolkit(this IServiceCollection services, RabbitSettings settings)
+        {
+            ArgumentNullException.ThrowIfNull(settings);
+            
+            services.AddScoped((provider) => settings);
+            
+            services.AddScoped<IQueueRequest, QueueRequest>();
+            services.AddScoped<IRabbitRequest, RabbitRequest>();
+            
+            return services;
+        }
+    }
+}
