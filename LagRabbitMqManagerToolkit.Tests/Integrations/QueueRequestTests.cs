47ab9af113784565ddf9b3e31e7a4a242b0e7e6b
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2025-04-12 08:03:27 -0300

    feat: Implementação de testes XUnit

---
+++ LagRabbitMqManagerToolkit.Tests/Integrations/QueueRequestTests.cs
@@ -0,0 +1,27 @@
+﻿using LagRabbitMqManagerToolkit.Domains;
+using LagRabbitMqManagerToolkit.Requests;
+using LagRabbitMqManagerToolkit.Requests.Interfaces;
+using Xunit;
+
+namespace LagRabbitMqManagerToolkit.Tests.Integrations
+{
+    public class QueueRequestTests
+    {
+        private readonly IQueueRequest _queueRequest;
+
+        public QueueRequestTests()
+        {
+            var settings = RabbitSettings.Default();
+
+            _queueRequest = new QueueRequest(settings);
+        }
+
+        [Fact]
+        public async Task GetAsync_WithValidQueueName_ReturnsExpectedResultAsync()
+        {
+            var queue = await _queueRequest.GetAsync("/", "error");
+
+            Assert.NotNull(queue);
+        }
+    }
+}
--- LagRabbitMqManagerToolkit/Domains/RabbitSettings.cs
+++ LagRabbitMqManagerToolkit/Domains/RabbitSettings.cs
@@ -7,4 +7,11 @@ public class RabbitSettings
     public required string Username { get; init; }
 
     public required string Password { get; init; }
+
+    public static RabbitSettings Default() => new()
+    {
+        Username = "guest",
+        Password = "guest",
+        Url = "http://localhost:15672/"
+    };
 }
