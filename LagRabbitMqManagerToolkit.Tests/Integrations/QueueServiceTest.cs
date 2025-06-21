d81646eb434d46f20ac04c46f7c7dd15d02f8143
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2025-06-21 05:08:21 -0300

    chore: Remoção dos testes

--- LagRabbitMqManagerToolkit.Tests/Integrations/QueueServiceTest.cs
+++ LagRabbitMqManagerToolkit.Tests/Integrations/QueueServiceTest.cs
@@ -1,7 +1,6 @@
 ﻿using LagRabbitMqManagerToolkit.Domains;
 using LagRabbitMqManagerToolkit.Services;
 using LagRabbitMqManagerToolkit.Services.Interfaces;
-using Xunit;
 
 namespace LagRabbitMqManagerToolkit.Tests.Integrations
 {
@@ -13,38 +12,10 @@ namespace LagRabbitMqManagerToolkit.Tests.Integrations
         public QueueServiceTest()
         {
             var settings = RabbitSettings.Default();
+            var requestService = new RequestService();
 
-            _queueRequest = new QueueService(settings);
-            _exchangeRequest = new ExchangeService(settings);
-        }
-
-        [Fact]
-        public async Task GetMessagesAsync_WithValidQueueName_ReturnsExpectedResultAsync()
-        {
-            var messages = await _queueRequest.GetMessagesAsync("/", "");
-
-            Assert.NotEmpty(messages);
-        }
-
-        [Fact]
-        public async Task GetAsync_WithValidQueueName_ReturnsExpectedResultAsync()
-        {
-            var queue = await _queueRequest.GetAsync("/", "");
-
-            Assert.NotNull(queue);
-        }
-
-        [Fact]
-        public async Task PublishAsync()
-        {
-            var properties = new Dictionary<string, string>()
-            {
-                { "message_id", Guid.NewGuid().ToString() }
-            };
-
-            var payload = "<teste></teste>";
-
-            await _exchangeRequest.PublishAsync("/", "", properties, payload);
+            _queueRequest = new QueueService(requestService, settings);
+            _exchangeRequest = new ExchangeService(requestService, settings);
         }
     }
 }
